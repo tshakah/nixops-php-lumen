8225183fc9703505887da59cf05049db4f1331aa
Elisha Hastings <5149755+tshakah@users.noreply.github.com>
2020-01-30 17:35:25 +0000

    Add blog archive

--- lumen.nix
+++ lumen.nix
@@ -6,14 +6,14 @@ let
     version = "0.1.0";
     # Just use the tar in the same directory for the soruce. We could also use a remote link to do this, and verify the
     # tar's hash as well.
-    src = ./blog.tar;
+    src = ./blog.tar.gz;
     # As we just need to unpack the tar file we only need the installPhase - if compilation etc was needed we might have
     # more
     phases = [ "installPhase" ];
     # The install phase takes the src tar (`$src`) and unpacks it to `$out` - this is a path in the nix store
     installPhase = ''
       mkdir $out
-      tar -xvf $src -C $out
+      tar -zxvf $src -C $out
     '';
   });
 in
